227fa3293daee3d9a0626423aefc95b4d4cafc3e
Arlind Qosa <qosa0001@algonquincollege.com>
2020-10-25 02:55:30 -0400

    first commit

---
+++ Lab4/Data/SchoolCommunityContext.cs
@@ -0,0 +1,25 @@
+﻿using Lab4.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Data
+{
+    public class SchoolCommunityContext : DbContext
+    {
+        public SchoolCommunityContext(DbContextOptions<SchoolCommunityContext> options) : base(options) { }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Community> Community { get; set; }
+        public DbSet<CommunityMembership> CommunityMembership { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Studnet");
+            modelBuilder.Entity<Community>().ToTable("Community");
+            modelBuilder.Entity<CommunityMembership>().ToTable("CommunityMembership");
+            modelBuilder.Entity<CommunityMembership>().HasKey(c => new { c.CommunityID, c.StudentID });
+        }
+    }
+}
---
+++ Lab4/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)] 
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        
+        public string FullName { 
+            get { 
+                return LastName + ", " + FirstName; 
+            }
+        }
+
+        public ICollection<CommunityMembership> CommunityMembership { get; set; }
+    }
+}
